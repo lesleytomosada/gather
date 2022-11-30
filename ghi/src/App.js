d5ca52576f4ee90e31ff8ba7b59b4202126f74e2
Lesley <tomosadalesley@gmail.com>
2022-11-29 16:16:57 -0800

    need to pull from main

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './Nav';
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
-import GatheringDetail from './GatheringDetail';
-import GatheringForm from './GatheringForm';
+// import Nav from './Nav';
+// import LoginForm from './LoginForm';
+// import SignupForm from './SignupForm';
+// import GatheringDetail from './GatheringDetail';
+// import GatheringForm from './GatheringForm';
 import GatheringList from './GatheringList';
-import PreferenceForm from './PreferenceForm';
-import MainPage from './MainPage';
+// import PreferenceForm from './PreferenceForm';
+// import MainPage from './MainPage';
 import './App.css';
 
 function App() {
@@ -35,17 +35,17 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Nav />
+      {/* <Nav /> */}
       <div className="container">
         <Routes>
-          <Route path="/" element={<MainPage />} />
+          {/* <Route path="/" element={<MainPage />} />
           <Route path="login" element={<LoginForm/>} />
-          <Route path="signup" element={<SignupForm/>} />
+          <Route path="signup" element={<SignupForm/>} /> */}
           <Route path="gathering/">
-            <Route path="new" element={<GatheringForm/>} />
+            {/* <Route path="new" element={<GatheringForm/>} /> */}
             <Route path="list" element={<GatheringList />} />
-          <Route path="gathering/<int:pk>" element={<GatheringDetail/>} />
-            <Route path="/preference" element={<PreferenceForm/>} />
+          {/* <Route path="gathering/<int:pk>" element={<GatheringDetail/>} />
+            <Route path="/preference" element={<PreferenceForm/>} /> */}
           </Route>
         </Routes>
       </div>
---
+++ ghi/src/GatheringList.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+
+const GatheringList = () => {
+
+    
+}
+
+export default GatheringList
