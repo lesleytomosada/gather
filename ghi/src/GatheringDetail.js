a90e35337558c98d9cb63bacb516daa116090a41
McKenzie Barker <mj.barker89@outlook.com>
2022-11-30 16:39:42 -0800

    fixed date

--- ghi/src/GatheringDetail.js
+++ ghi/src/GatheringDetail.js
@@ -21,8 +21,6 @@ const GatheringDetail = () => {
 
       if (response.ok) {
         let data = await response.json();
-        console.log('data: ', data)
-        console.log('time: ', new Date(gathering.date).toLocaleTimeString("en-US", {TimeZone: "America/Los_Angeles"}))
         setGathering(data)
       }
     }
@@ -35,11 +33,11 @@ const GatheringDetail = () => {
       <h1 className="display-4 fw-bold text-center">Your Gathering</h1>
         <h2>{gathering.name}</h2>
         {new Date(gathering.date).toLocaleDateString()}&nbsp;
-        {new Date(gathering.date).toLocaleTimeString()}
+        {new Date(gathering.date + 'Z').toLocaleTimeString()}
         <h3>{gathering.location}</h3>
         <h3>{gathering.recommendation?.restaurant_name}</h3>
         <h3>{gathering.recommendation?.address}</h3>
-        <img style={{width: "50%", height: "50%"}}src={gathering.recommendation?.image_url} alt='food from Le Pichet' />
+        <img style={{width: "50%", height: "50%"}}src={gathering.recommendation?.image_url} alt='recommended restaurant' />
         <p>
           <button onClick={navToPreference}>Add a preference</button>
         </p>
