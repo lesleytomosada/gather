832841a225da9ee1ae5ac957ff05cfc082839eeb
Cindy Chiang <chingychiang@gmail.com>
2022-11-30 23:26:54 -0800

    updated form with auth and useNavigate

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -10,29 +10,6 @@ import MainPage from './MainPage';
 import './App.css';
 
 function App() {
-  // const [launch_info, setLaunchInfo] = useState([]);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   async function getData() {
-  //     let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-  //     console.log('fastapi url: ', url);
-  //     let response = await fetch(url);
-  //     console.log("------- hello? -------");
-  //     let data = await response.json();
-
-  //     if (response.ok) {
-  //       console.log("got launch data!");
-  //       setLaunchInfo(data.launch_details);
-  //     } else {
-  //       console.log("drat! something happened");
-  //       setError(data.message);
-  //     }
-  //   }
-  //   getData();
-  // }, [])
-
-
   return (
     <BrowserRouter>
       {/* <Nav /> */}
