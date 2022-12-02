212938c26ddc227a059112b60357343f917d7a47
daisy song <daisysong19900215@gmail.com>
2022-12-02 22:56:14 +0000

    auth frontend works

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,57 +1,43 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './Nav';
-import LoginForm from './LoginForm';
-import SignupForm from './SignupForm';
-import GatheringDetail from './GatheringDetail';
-import GatheringForm from './GatheringForm';
-import GatheringList from './GatheringList';
-import PreferenceForm from './PreferenceForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Nav from './Nav';
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+// import GatheringDetail from "./GatheringDetail";
+// import GatheringForm from "./GatheringForm";
+// import GatheringList from "./GatheringList";
+// import PreferenceForm from "./PreferenceForm";
 // import MainPage from './MainPage';
-import './App.css';
+import "./App.css";
+import { AuthProvider, useToken } from "./auth";
+
+function GetToken() {
+  useToken();
+  return null;
+}
 
 function App() {
-  // const [launch_info, setLaunchInfo] = useState([]);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   async function getData() {
-  //     let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-  //     console.log('fastapi url: ', url);
-  //     let response = await fetch(url);
-  //     console.log("------- hello? -------");
-  //     let data = await response.json();
-
-  //     if (response.ok) {
-  //       console.log("got launch data!");
-  //       setLaunchInfo(data.launch_details);
-  //     } else {
-  //       console.log("drat! something happened");
-  //       setError(data.message);
-  //     }
-  //   }
-  //   getData();
-  // }, [])
-
-
   return (
     <BrowserRouter>
-      <Nav />
-      <div className="container">
-        <Routes>
-          {/* <Route path="/" element={<MainPage />} /> */}
-          <Route path="login" element={<LoginForm />} />
-          <Route path="signup" element={<SignupForm />} />
-          <Route path="gathering/">
-            <Route path="new" element={<GatheringForm />} />
-            <Route path="list" element={<GatheringList />} />
-            <Route path="<int:pk>">
-              <Route path="" element={<GatheringDetail />} />
-              <Route path="preference" element={<PreferenceForm />} />
-            </Route>
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <GetToken />
+        {/* <Nav /> */}
+        <div className="container">
+          <Routes>
+            {/* <Route path="/" element={<MainPage />} /> */}
+            <Route path="login" element={<LoginForm />} />
+            <Route path="signup" element={<SignupForm />} />
+            {/* <Route path="gathering/">
+              <Route path="new" element={<GatheringForm />} />
+              <Route path="list" element={<GatheringList />} />
+              <Route path="<int:pk>">
+                <Route path="" element={<GatheringDetail />} />
+                <Route path="preference" element={<PreferenceForm />} />
+              </Route>
+            </Route> */}
+          </Routes>
+        </div>
+    </AuthProvider>
+  </BrowserRouter>
   );
 }
 
