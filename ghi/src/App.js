57023e2dc27b7da870a8b2d1b33ba60f218628d4
McKenzie Barker <mj.barker89@outlook.com>
2022-12-02 15:27:34 -0800

    fixed route paths and uncommented things

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Nav from './Nav';
+import Nav from './Nav';
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
-// import GatheringDetail from "./GatheringDetail";
-// import GatheringForm from "./GatheringForm";
-// import GatheringList from "./GatheringList";
-// import PreferenceForm from "./PreferenceForm";
-// import MainPage from './MainPage';
+import GatheringDetail from "./GatheringDetail";
+import GatheringForm from "./GatheringForm";
+import GatheringList from "./GatheringList";
+import PreferenceForm from "./PreferenceForm";
+import MainPage from './MainPage';
 import "./App.css";
 import { AuthProvider, useToken } from "./auth";
 
@@ -20,20 +20,20 @@ function App() {
     <BrowserRouter>
     <AuthProvider>
       <GetToken />
-        {/* <Nav /> */}
+        <Nav />
         <div className="container">
           <Routes>
-            {/* <Route path="/" element={<MainPage />} /> */}
+            <Route path="/" element={<MainPage />} />
             <Route path="login" element={<LoginForm />} />
             <Route path="signup" element={<SignupForm />} />
-            {/* <Route path="gathering/">
+            <Route path="gathering/">
               <Route path="new" element={<GatheringForm />} />
               <Route path="list" element={<GatheringList />} />
-              <Route path="<int:pk>">
+              <Route path=":gathering_id/">
                 <Route path="" element={<GatheringDetail />} />
                 <Route path="preference" element={<PreferenceForm />} />
               </Route>
-            </Route> */}
+            </Route>
           </Routes>
         </div>
     </AuthProvider>
