863001838294dc336b3262fd70f614c8f722a8a5
McKenzie Barker <mj.barker89@outlook.com>
2022-12-02 15:29:46 -0800

    app.js fixed

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Nav from './Nav';
-// import LoginForm from './LoginForm';
-// import SignupForm from './SignupForm';
-import GatheringDetail from './GatheringDetail';
-// import GatheringForm from './GatheringForm';
-// import GatheringList from './GatheringList';
-// import PreferenceForm from './PreferenceForm';
-// import MainPage from './MainPage';
-import './App.css';
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from './Nav';
 import LoginForm from "./LoginForm";
@@ -26,51 +14,27 @@ function GetToken() {
   useToken();
   return null;
 }
->>>>>>> main
 
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      {/* <Nav /> */}
+      <Nav />
       <div className="container">
         <Routes>
-          {/* <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<MainPage />} />
           <Route path="login" element={<LoginForm />} />
-          <Route path="signup" element={<SignupForm />} /> */}
+          <Route path="signup" element={<SignupForm />} />
           <Route path="gathering/">
-            {/* <Route path="new" element={<GatheringForm />} />
-            <Route path="list" element={<GatheringList />} /> */}
-            <Route path=":id">
+            <Route path="new" element={<GatheringForm />} />
+            <Route path="list" element={<GatheringList />} />
+            <Route path=":gathering_id/">
               <Route path="" element={<GatheringDetail />} />
-              {/* <Route path="preference" element={<PreferenceForm />} /> */}
+              <Route path="preference" element={<PreferenceForm />} />
             </Route>
           </Route>
         </Routes>
       </div>
     </BrowserRouter>
-=======
-    <AuthProvider>
-      <GetToken />
-        <Nav />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="login" element={<LoginForm />} />
-            <Route path="signup" element={<SignupForm />} />
-            <Route path="gathering/">
-              <Route path="new" element={<GatheringForm />} />
-              <Route path="list" element={<GatheringList />} />
-              <Route path=":gathering_id/">
-                <Route path="" element={<GatheringDetail />} />
-                <Route path="preference" element={<PreferenceForm />} />
-              </Route>
-            </Route>
-          </Routes>
-        </div>
-    </AuthProvider>
-  </BrowserRouter>
->>>>>>> main
   );
 }
 
