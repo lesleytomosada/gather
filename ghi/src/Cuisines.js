9e91d681692516eebbe8999d88eda7e3afd3efcb
cindyschiang <chingychiang@gmail.com>
2022-12-03 00:16:21 +0000

    Preference form component

---
+++ ghi/src/Cuisines.js
@@ -0,0 +1,82 @@
+export const cuisines = [
+    {
+        name: "American (New)",
+        alias: "newamerican",
+    },
+    {
+        name: "American (Traditional)",
+        alias: "tradamerican",
+    },
+    {
+        name: "Barbeque",
+        alias: "bbq",
+    },
+    {
+        name: "Breakfast & Brunch",
+        alias: "breakfast_brunch",
+    },
+    {
+        name: "Chinese",
+        alias: "chinese",
+    },
+    {
+        name: "French",
+        alias: "french",
+    },
+    {
+        name: "German",
+        alias: "german",
+    },
+    {
+        name: "Greek",
+        alias: "greek",
+    },
+    {
+        name: "Indian",
+        alias: "indpak",
+    },
+    {
+        name: "Italian",
+        alias: "italian",
+    },
+    {
+        name: "Japanese",
+        alias: "japanese",
+    },
+    {
+        name: "Korean",
+        alias: "korean",
+    },
+    {
+        name: "Mediterranean",
+        alias: "mediterranean",
+    },
+    {
+        name: "Mexican",
+        alias: "mexican",
+    },
+    {
+        name: "Pizza",
+        alias: "pizza",
+    },
+    {
+        name: "Sandwiches",
+        alias: "sandwiches",
+    },
+    {
+        name: "Spanish",
+        alias: "spanish",
+    },
+    {
+        name: "Thai",
+        alias: "thai",
+    },
+    {
+        name: "Vegetarian",
+        alias: "vegetarian",
+    },
+    {
+        name: "Vietnamese",
+        alias: "vietnamese",
+    },
+];
