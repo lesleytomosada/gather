d9cc6ffa668d450e73c33a740d0b457c6a250bc7
McKenzie Barker <mj.barker89@outlook.com>
2022-12-05 12:56:29 -0800

    testing file name for deployment

--- gatherings/main.py
+++ gatherings/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import gathering, preference, recommendation
+from routers import gathering, preference, recommendation
 import os
 
 app = FastAPI()
