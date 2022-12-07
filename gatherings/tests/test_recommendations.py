bbba34d348035687aa664b3bd99826a36f3bb721
Cindy Chiang <chingychiang@gmail.com>
2022-12-06 21:26:29 -0800

    fixed lint errors and added srv to queries files

--- gatherings/tests/test_recommendations.py
+++ gatherings/tests/test_recommendations.py
@@ -1,7 +1,6 @@
-from routers.recommendation import get_min_price
+# from routers.recommendation import get_min_price
 
 
-def test_get_min_price():
-  input = [2, 3, 4, 1]
-  target = 1
-  
+# def test_get_min_price():
+#     input = [2, 3, 4, 1]
+#     target = 1
