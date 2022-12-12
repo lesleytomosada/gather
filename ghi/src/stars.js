ebe7efc71a5d40b0f2ccfcccabaf9ead3460b446
McKenzie Barker <mj.barker89@outlook.com>
2022-12-12 11:50:05 -0800

    playing with pics

--- ghi/src/stars.js
+++ ghi/src/stars.js
@@ -10,16 +10,16 @@ import regular_2_half from "./assets/regular_2_half.png";
 import regular_1 from "./assets/regular_1.png"
 
 const star = {
-  0: { regular_0 },
-  1: { regular_1 },
-  1.5: { regular_1_half },
-  2: { regular_2 },
-  2.5: { regular_2_half },
-  3: { regular_3 },
-  3.5: { regular_3_half },
-  4: { regular_4 },
-  4.5: { regular_4_half },
-  5: { regular_5 },
+  0: regular_0,
+  1: regular_1,
+  1.5: regular_1_half,
+  2: regular_2,
+  2.5: regular_2_half,
+  3: regular_3,
+  3.5: regular_3_half,
+  4: regular_4,
+  4.5: regular_4_half,
+  5: regular_5,
 };
 
 export default star;
