f8527d812f2f9d6112b6d2a1aba5aee1cc1c07af
McKenzie Barker <mj.barker89@outlook.com>
2022-12-12 10:27:47 -0800

    mongo strings

--- gatherings/queries/gathering_queries.py
+++ gatherings/queries/gathering_queries.py
@@ -13,7 +13,7 @@ dbname = os.environ["MONGODATABASE"]
 dbuser = os.environ["MONGOUSER"]
 dbpass = os.environ["MONGOPASSWORD"]
 
-mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
+mongo_str = f"mongodb+srv://{dbuser}:{dbpass}@{dbhost}"
 
 client = pymongo.MongoClient(mongo_str)
 
--- gatherings/queries/recommendation_queries.py
+++ gatherings/queries/recommendation_queries.py
@@ -9,7 +9,7 @@ dbname = os.environ["MONGODATABASE"]
 dbuser = os.environ["MONGOUSER"]
 dbpass = os.environ["MONGOPASSWORD"]
 
-mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
+mongo_str = f"mongodb+srv://{dbuser}:{dbpass}@{dbhost}"
 
 client = pymongo.MongoClient(mongo_str)
 
